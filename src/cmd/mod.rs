54262ca4b1fd1837b67ef16811a74c6795316431
jdessoli <julien.dessoliers@hotmail.com>
2026-05-15 21:44:46 +0200

    fix(mod): updating mod to match new features

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -9,5 +9,5 @@ pub use cmd_convert::cmd_convert;
 pub use cmd_editor_setup::cmd_editor_setup;
 pub use cmd_fmt::cmd_fmt;
 pub use cmd_init::cmd_init;
-pub use cmd_misc::{cmd_install, cmd_update, cmd_check, cmd_stdlib, run_lsp};
+pub use cmd_misc::{cmd_update, cmd_check, cmd_stdlib, run_lsp};
 pub use cmd_prod::cmd_prod;
